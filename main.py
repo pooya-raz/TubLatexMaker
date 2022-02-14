4a4d40b7cf43181edc7798bd9dc09c5125a3bd4d
pooya <pooya72@gmail.com>
2022-02-14 18:30:30 +0000

    Split class into own file

--- main.py
+++ main.py
@@ -3,38 +3,12 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import logging
-
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
-
 import requests
 from typing import Protocol
+from tub_latex_maker.services.tub_media_wiki_service import TubMediaWikiService
 
 
-class MediaWikiAPIService(Protocol):
-    """Consumes MediaWiki API"""
-
-    def __init__(self):
-        ...
-
-    def semantic_search(self, query: str) -> dict:
-        ...
-
-
-class TubMediaWikiService:
-    url = "http://144.173.140.108:8080/tub"
-    headers = {"user-agent": "my-app/0.0.1"}
-
-    def __init__(self, http_library):
-        self.http_library = http_library
-
-    def semantic_search(self, semantic_query: str) -> dict:
-        logging.info("Connecting to:" + self.url)
-        response = self.http_library.get(
-            self.url + "/api.php?action=ask&format=json&query=" + semantic_query,
-            headers=self.headers,
-        )
-        logging.info("Connection status: Successful")
-        return response.json()["query"]["results"]
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 
 
 class DictToText(Protocol):
---
+++ tub_latex_maker/services/tub_media_wiki_service.py
@@ -0,0 +1,29 @@
+import logging
+from typing import Protocol, runtime_checkable
+
+
+class MediaWikiAPIService(Protocol):
+    """Consumes MediaWiki API"""
+
+    def __init__(self):
+        ...
+
+    def semantic_search(self, query: str) -> dict:
+        ...
+
+
+class TubMediaWikiService:
+    url = "http://144.173.140.108:8080/tub"
+    headers = {"user-agent": "my-app/0.0.1"}
+
+    def __init__(self, http_library):
+        self.http_library = http_library
+
+    def semantic_search(self, semantic_query: str) -> dict:
+        logging.info("Connecting to:" + self.url)
+        response = self.http_library.get(
+            self.url + "/api.php?action=ask&format=json&query=" + semantic_query,
+            headers=self.headers,
+        )
+        logging.info("Connection status: Successful")
+        return response.json()["query"]["results"]
