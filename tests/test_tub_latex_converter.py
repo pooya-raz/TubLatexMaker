4d9cc909d1798add6c2c0b109d5b702d8f34f467
Pooya Razavian <pooya72@gmail.com>
2022-02-18 08:33:36 +0000

    Programmatically create the expected test case

--- tests/test_tub_latex_converter.py
+++ tests/test_tub_latex_converter.py
@@ -15,6 +15,40 @@ entry = {
 }
 
 
+def create_expected_latex(transliterated_title: str, arabic_title: str) -> str:
+    return f"""
+    \\item \\textbf{{{transliterated_title}}}
+    {arabic_title}
+    \\newline
+    Muḥammad b. Faraj al-Ḥimyarī al-Najafī
+    \\newline
+    (1059/1649)
+    \\newline
+    \\newline
+    \\textbf{{Description}}
+
+    \\newline	
+    A short one-line description.
+    \\newline
+    \\newline
+    \\textbf{{Principle manuscripts}}
+    \\newline
+    This manuscript
+    \\newline
+    \\newline
+    \\textbf{{Editions}}
+    \\newline
+    This edition.
+    \\newline
+    \\newline
+    \\textbf{{Commentaries}}
+    \\newline
+    This commentary.
+    \\newline
+    """
+
+
 def test_convert_to_entry():
     entry_values = list(entry.values())[0]["printouts"]
-    assert convert.to_entry_with_commentary(entry_values) == "hello"
+    expected = create_expected_latex("(Bahth fī) uṣūl al-fiqh", "بحث في) أصول الفقه)")
+    assert convert.to_entry_with_commentary(entry_values) == expected
