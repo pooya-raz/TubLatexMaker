30a718c5938512cb1173430fea776f4f0f22a8bf
Pooya Razavian <pooya72@gmail.com>
2022-02-18 16:06:18 +0000

    Format arabic text correctly

--- tublatexmaker/tub_latex_converter.py
+++ tublatexmaker/tub_latex_converter.py
@@ -1,35 +1,36 @@
 def to_entry_with_commentary(entry: dict) -> str:
     transliterated_title = "".join(entry["Title (transliterated)"])
-    arabic_title = "".join(entry["Title (Arabic)"])
+    arabic_title = entry["Title (Arabic)"][0]
     author = "".join(entry["Has author(s)"][0]["fulltext"])
     return f"""
-    \\item \\textbf{{{transliterated_title}}}
-    {arabic_title}
-    \\newline
-    {author}
-    \\newline
-    (1059/1649)
-    \\newline
-    \\newline
-    \\textbf{{Description}}
-    \\newline	
-    A short one-line description.
-    \\newline
-    \\newline
-    \\textbf{{Principle manuscripts}}
-    \\newline
-    This manuscript
-    \\newline
-    \\newline
-    \\textbf{{Editions}}
-    \\newline
-    This edition.
-    \\newline
-    \\newline
-    \\textbf{{Commentaries}}
-    \\newline
-    This commentary.
-    \\newline
+      \item \\textbf{{{transliterated_title}}}
+        \\newline
+        \\textarabic{{{arabic_title}}}
+        \\newline
+        {author}
+        \\newline
+        (1059/1649)
+        \\newline
+        \\newline
+        \\textbf{{Description}}
+        \\newline	
+        A short one-line description.
+        \\newline
+        \\newline
+        \\textbf{{Principle manuscripts}}
+        \\newline
+        This manuscript
+        \\newline
+        \\newline
+        \\textbf{{Editions}}
+        \\newline
+        This edition.
+        \\newline
+        \\newline
+        \\textbf{{Commentaries}}
+        \\newline
+        This commentary.
+        \\newline
     """
 
 
