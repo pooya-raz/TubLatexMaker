cbaede7cf0258ddd3d8653d131a8f34710c3a001
Pooya Razavian <pooya72@gmail.com>
2022-02-18 11:15:36 +0000

    Query for monographs without commentaries

--- tublatexmaker/main.py
+++ tublatexmaker/main.py
@@ -8,8 +8,11 @@ import os.path
 
 def main():
     tub_mediawiki_service = TubMediaWikiService(requests)
-    query = "[[Category:Title]]|?Title (Arabic)|?Title (transliterated)|limit=5"
-    list_of_entries = tub_mediawiki_service.semantic_search(query)
+    query_monograph_no_commentaries = "[[Category:Title]][[Book type::Monograph]][[Has number of commentaries::0]]|?Title (Arabic)|?Title (transliterated)|?Has author(s)"
+    list_of_entries = tub_mediawiki_service.semantic_search(
+        query_monograph_no_commentaries
+    )
+    print(list_of_entries)
     latex_of_entries = [to_entry_with_commentary(entry) for entry in list_of_entries]
     write_to_file("output", "".join(latex_of_entries))
 
