4172f745906382139962d1129d027b251c3d3d9e
Pooya Razavian <pooya72@gmail.com>
2022-02-18 16:27:23 +0000

    Complete the latex output by adding the pre and post commands
    These commands make it a complete LaTeX document that can now me run by XeLaTeX.

--- tublatexmaker/main.py
+++ tublatexmaker/main.py
@@ -1,20 +1,24 @@
 from pprint import pprint
 import requests
 from services.tub_media_wiki_service import TubMediaWikiService
-from tub_latex_converter import to_entry_with_commentary
+from tub_latex_converter import *
 from services.file_writer_service import write_to_file
 import os.path
 
 
 def main():
     tub_mediawiki_service = TubMediaWikiService(requests)
-    query_monograph_no_commentaries = "[[Category:Title]][[Book type::Monograph]][[Has number of commentaries::0]]|?Title (Arabic)|?Title (transliterated)|?Has author(s)"
+    query_monograph_no_commentaries = "[[Category:Title]][[Book type::Monograph]][[Has number of commentaries::0]]|?Title (Arabic)|?Title (transliterated)|?Has author(s)|limit=1000"
     list_of_entries = tub_mediawiki_service.semantic_search(
         query_monograph_no_commentaries
     )
     print(list_of_entries)
-    latex_of_entries = [to_entry_with_commentary(entry) for entry in list_of_entries]
-    write_to_file("output", "".join(latex_of_entries))
+    latex_of_entries = "".join(
+        [to_entry_with_commentary(entry) for entry in list_of_entries]
+    )
+    monographs_without_commentary = wrap_monograph_without_commentary(latex_of_entries)
+    document = wrap_document(monographs_without_commentary)
+    write_to_file("output", document)
 
 
 if __name__ == "__main__":
--- tublatexmaker/tub_latex_converter.py
+++ tublatexmaker/tub_latex_converter.py
@@ -34,6 +34,36 @@ def to_entry_with_commentary(entry: dict) -> str:
     """
 
 
+def add_pre_and_post_commands(pre: str, latex_body: str, post: str) -> str:
+    return pre + latex_body + post
+
+
+def wrap_monograph_without_commentary(latex_body: str) -> str:
+    return add_pre_and_post_commands(
+        "\\begin{enumerate}", latex_body, "\\end{enumerate}"
+    )
+
+
+def wrap_document(latex_body: str) -> str:
+    pre = """
+    \\documentclass{article}
+    \\usepackage{fontspec,lipsum}
+    \\defaultfontfeatures{Ligatures=TeX}
+    \\usepackage[small,sf,bf]{titlesec}
+    \\setromanfont{Gentium Plus}
+    \\newfontfamily\\arabicfont[]{Al Bayan}
+    \\usepackage{polyglossia}
+    \\setmainlanguage{english}
+    \\setotherlanguage{arabic}
+    \\begin{document}
+    """
+    post = """
+    \\end{document}
+    """
+
+    return add_pre_and_post_commands(pre, latex_body, post)
+
+
 def to_entry(dictionary: dict):
     for entry in dictionary:
         return to_entry_with_commentary(entry)
