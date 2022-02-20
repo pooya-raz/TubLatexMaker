3a2b0f1bcd6b546c2b9f71765a687396f7cec23d
pooya <pooya72@gmail.com>
2022-02-20 17:13:21 +0000

    Encode in utf-8
    If utf-8 encoding is not specified, it will raise an error. This is most likely due to diacritics in the database.

--- tublatexmaker/services/file_writer_service.py
+++ tublatexmaker/services/file_writer_service.py
@@ -6,6 +6,6 @@ def write_to_file(filename: str, text: str) -> None:
     dirname = os.path.abspath(os.curdir)
     save_path = "/output/"
     complete_name = os.path.join(dirname + save_path, filename + ".tex")
-    file = open(complete_name, "w")
+    file = open(complete_name, "w", encoding="utf-8")
     file.write(text)
     file.close()
